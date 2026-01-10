775ad688b525f5ca07f5e8787f201225486c2ed6
RUSHIKESH CHAMLE <officialrushikesh22@gmail.com>
2026-01-10 11:19:05 +0530

    Implement minimum ASCII delete sum function

---
+++ 712. Minimum ASCII Delete Sum for Two Strings.py
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int minimumDeleteSum(char* s1, char* s2) {
+    int m = strlen(s1);
+    int n = strlen(s2);
+
+    int dp[m + 1][n + 1];
+
+    dp[0][0] = 0;
+
+    for (int i = 1; i <= m; i++)
+        dp[i][0] = dp[i - 1][0] + s1[i - 1];
+
+    for (int j = 1; j <= n; j++)
+        dp[0][j] = dp[0][j - 1] + s2[j - 1];
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (s1[i - 1] == s2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                int delete1 = dp[i - 1][j] + s1[i - 1];
+                int delete2 = dp[i][j - 1] + s2[j - 1];
+                dp[i][j] = delete1 < delete2 ? delete1 : delete2;
+            }
+        }
+    }
+
+    return dp[m][n];
+}
