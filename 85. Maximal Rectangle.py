c7b85a0ad530982540b09c972d7e8608c26dd893
RUSHIKESH CHAMLE <officialrushikesh22@gmail.com>
2026-01-11 16:10:59 +0530

    Implement maximal rectangle algorithm in C

---
+++ 85. Maximal Rectangle.py
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+
+int largestRectangleArea(int* heights, int n) {
+    int stack[205];
+    int top = -1;
+    int maxArea = 0;
+
+    for (int i = 0; i <= n; i++) {
+        int h = (i == n) ? 0 : heights[i];
+        while (top != -1 && h < heights[stack[top]]) {
+            int height = heights[stack[top--]];
+            int width = (top == -1) ? i : i - stack[top] - 1;
+            int area = height * width;
+            if (area > maxArea) maxArea = area;
+        }
+        stack[++top] = i;
+    }
+    return maxArea;
+}
+
+int maximalRectangle(char** matrix, int matrixSize, int* matrixColSize) {
+    if (matrixSize == 0 || matrixColSize[0] == 0) return 0;
+
+    int cols = matrixColSize[0];
+    int heights[205];
+    for (int i = 0; i < cols; i++) heights[i] = 0;
+
+    int maxArea = 0;
+
+    for (int i = 0; i < matrixSize; i++) {
+        for (int j = 0; j < cols; j++) {
+            if (matrix[i][j] == '1')
+                heights[j]++;
+            else
+                heights[j] = 0;
+        }
+        int area = largestRectangleArea(heights, cols);
+        if (area > maxArea) maxArea = area;
+    }
+    return maxArea;
+}
